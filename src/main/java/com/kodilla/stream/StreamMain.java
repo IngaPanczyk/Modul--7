abaeb60dbfbd8c2cf5a0ea1f2f62da2f51c1bdb9
IngaPanczyk <ingawaaa@gmail.com>
2020-06-16 21:45:45 +0200

    Zadanie 2.0

--- src/main/java/com/kodilla/stream/StreamMain.java
+++ src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.lambda.Processor;
 import com.kodilla.stream.reference.FunctionalCalculator;
@@ -29,5 +30,7 @@ public class StreamMain {
         poemBeautifier.beautify("This is the most beautiful poem in the world", (poem) -> poem.toUpperCase());
         poemBeautifier.beautify("This is the most beautiful poem in the world", (poem) -> poem.toLowerCase());
         poemBeautifier.beautify("This is the most beautiful poem in the world", (poem) -> poem.replace("beautiful", "beautiful beautiful beautiful"));
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
