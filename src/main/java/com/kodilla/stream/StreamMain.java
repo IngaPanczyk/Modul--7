ecc71237530b976f85efdc7a4697edc1abacea9d
IngaPanczyk <ingawaaa@gmail.com>
2020-06-16 21:06:01 +0200

    Zadanie 1.1

--- src/main/java/com/kodilla/stream/StreamMain.java
+++ src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,7 +1,32 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.lambda.Processor;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
 public class StreamMain {
     public static void main(String[] args) {
-        System.out.println("Welcome to module 7 - Stream");
+        Processor processor = new Processor();
+        processor.execute(() -> System.out.println("This is an example text."));
+
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+        //Zadanie 1
+        System.out.println("After beautify");
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.beautify("This is the most beautiful poem in the world", (poem) -> poem + " +++ more beautyfull");
+        poemBeautifier.beautify("This is the most beautiful poem in the world", (poem) -> poem.toUpperCase());
+        poemBeautifier.beautify("This is the most beautiful poem in the world", (poem) -> poem.replace("beautiful","beautiful beautiful beautiful"));
     }
 }
