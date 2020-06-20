c39a10ef31b33336c3dcdd1f0ad08bb39b207f7a
IngaPanczyk <ingawaaa@gmail.com>
2020-06-20 21:50:26 +0200

    Zadanie 5.0

--- src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,4 +1,5 @@
-/*
+
+
 package com.kodilla.stream.array;
 
 import java.util.OptionalDouble;
@@ -7,14 +8,18 @@ import java.util.stream.IntStream;
 public interface ArrayOperations {
 
     static double getAverage(int[] numbers) {
-        */
-/*double show = IntStream.range(0, numbers.length)
-                .forEach(System.out::println);*//*
-
-
+        /*IntStream.range(0, numbers.length)
+                .map(n -> numbers[n])
+                .forEach(System.out::println);*/
+        //tworzę zmienną average, tworzę strumień elementów typu int o zakresach numerów od 0 do numbers.length
         double average = IntStream.range(0, numbers.length)
+                //dokonuję transformacji, do każdego numeru przypisany jest numer z tablicy numers
+                .map(n -> numbers[n])
+                // liczona jest średnia z elementów tablicy numbers
                 .average();
         return average;
+
     }
 }
-*/
+
+
--- src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -1,4 +1,4 @@
-/*package com.kodilla.stream.array;
+package com.kodilla.stream.array;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -7,16 +7,16 @@ public class ArrayOperationsTestSuite implements ArrayOperations {
     @Test
     public void testGetAverage() {
         //Given
+        //tworzę tablicę numerów
         int[] numbers = new int[20];
-
-        for (int i = 0; i < 10; i++)
+        //w pętli wypełniam ją liczbami od 1-20
+        for (int i = 0; i < 20; i++)
             numbers[i] = i + 1;
-
         //When
-        double average = ArrayOperations.getAverage(numbers);
+        //Wywołuję metodę getAverage na obiekcie numbers
+        double average = getAverage(numbers);
         //Then
-        Assert.assertArrayEquals(3.0,average);
-
-
+        //Sprawdzam policzoną średnią
+        Assert.assertEquals(3.0,average,0);
     }
-}*/
+}
